8cef631fc1df22966ab879e7a6eeb30c958287bb
Brandon Lim Khin Wai <brandon.lim0499@gmail.com>
2022-02-27 16:31:58 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
+// import SpotifyIcon from "./components/icons/SpotifyLogo.vue";
+// import HomeIcon from "./components/icons/HomeIcon.vue";
+</script>
+
+<template>
+  <!-- <header>
+    <nav>
+      <RouterLink to="/" class="text-indigo-600">Home </RouterLink>
+      <RouterLink to="/search" class="text-indigo-600">Search</RouterLink>
+    </nav>
+  </header> -->
+  <div class="flex h-[130vh]">
+    <Navbar></Navbar>
+
+    <!-- main right side -->
+    <main class="w-screen h-screen">
+      <Header></Header>
+      <div class="border border-red-500 p-5 mt-0">Hello</div>
+    </main>
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <!--header for sign up and login-->
+  <div
+    class="w-full bg-black bg-opacity-[.89] flex items-center justify-between px-4 py-[14px] sticky top-0"
+  >
+    <!-- div for back & next button -->
+    <div class="flex w-fit space-x-2">
+      <button class="bg-black p-1 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+
+      <button class="bg-black p-1 rounded-full opacity-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+    </div>
+
+    <!-- container for sign up & log in -->
+    <div class="flex space-x-2 whitespace-nowrap">
+      <button
+        class="bg-inherit text-white cursor-default font-bold uppercase text-xs px-9 py-[10px] rounded-3xl tracking-[1.76px] hover:scale-105"
+      >
+        Sign Up
+      </button>
+      <button
+        class="bg-white text-black font-bold uppercase text-xs px-9 py-[10px] rounded-3xl tracking-[1.76px] hover:scale-105"
+      >
+        Log In
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
